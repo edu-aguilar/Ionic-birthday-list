f921dbbc05cf19d9df5cae1ff4635a240aa79d1d
Eduardo Aguilar <eduardoaguilartirado@gmail.com>
2016-07-11 12:25:46 +0200

    added styles & basic logic to newDate modal.

--- ionic-birthday-list/www/js/controllers/newDate.controller.js
+++ ionic-birthday-list/www/js/controllers/newDate.controller.js
@@ -6,13 +6,17 @@
         .controller('NewDateController', NewDateController);
 
     /* @ngInject */
-    function NewDateController() {
+    function NewDateController(newDateService) {
         var vm = this;
 
         activate();
 
         function activate() {
-            
+            vm.closeDateModal = closeDateModal;
+        }
+
+        function closeDateModal() {
+            var modal = newDateService.getModal().hide();
         }
     }
 })();
